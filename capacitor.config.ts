881902ee09b8e2d368a540929a28e6a18203d171
ToastedBaguette <ronyhartonoirawan@gmail.com>
2022-12-22 22:23:30 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HMPUAS',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
